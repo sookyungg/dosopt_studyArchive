dcef659f7ed957b5765517744543cda3ea39d146
Sookyung Kang <thoosoo@naver.com>
2023-10-17 21:44:49 +0900

    단속카메라 해결

---
+++ KSK/그리디/prog_level3_단속카메라.py
@@ -0,0 +1,11 @@
+def solution(routes):
+    answer = 0
+    routes.sort(key=lambda x:x[1])
+    camera = -300001
+    
+    for route in routes:
+        if camera<route[0]:
+            answer+=1
+            camera = route[1]
+          
+    return answer
