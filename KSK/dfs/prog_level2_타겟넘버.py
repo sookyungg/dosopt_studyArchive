ba6b4c38131f48c7b0892d4b6601d0f7963f19d4
Sookyung Kang <thoosoo@naver.com>
2023-10-19 13:06:47 +0900

    타겟 넘버 해결

---
+++ KSK/dfs/prog_level2_타겟넘버.py
@@ -0,0 +1,21 @@
+'''복습'''
+def solution(numbers, target):
+    answer = 0
+    n=len(numbers)
+    
+    def dfs(idx,numbers):
+        answer = 0
+        if idx ==n:
+            if sum(numbers) ==target:
+                return 1
+            else:
+                return 0
+        answer+=dfs(idx+1,numbers)
+        numbers[idx]*=-1
+        answer+=dfs(idx+1,numbers)
+        
+        return answer
+    
+    answer=dfs(0,numbers)
+    
+    return answer
